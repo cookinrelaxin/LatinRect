de5b35c0c6e44d25a560de2f5cdf354b466dbe43
Zachary Feldcamp <zachary.feldcamp@me.com>
2021-11-20 20:15:26 -0500

    Implemented the right column generation

--- latin_rectangle.py
+++ latin_rectangle.py
@@ -9,7 +9,26 @@ SSP5 	= 'SSP'
 SPP6 	= 'SPP'
 SSA7 	= 'SSA'
 SPA8 	= 'SPA'
-fields = [EXP0, SS1, SP2, SSS3, SPS4, SSP5, SPP6, SSA7, SPA8]
+
+combs = [
+			[SS1, 	SP2, 	SSS3, 	SPS4, 	SSP5, 	SPP6, 	SSA7, 	SPA8],
+			[SP2, 	SSS3, 	SPS4, 	SSP5, 	SPP6, 	SSA7, 	SPA8, 	SS1],
+			[SSS3, 	SPS4, 	SSP5, 	SPP6, 	SSA7, 	SPA8, 	SS1, 	SP2],
+			[SPS4, 	SSP5, 	SPP6, 	SSA7, 	SPA8, 	SS1, 	SP2, 	SSS3],
+			[SSP5, 	SPP6, 	SSA7, 	SPA8, 	SS1, 	SP2, 	SSS3, 	SPS4],
+			[SPP6, 	SSA7, 	SPA8, 	SS1, 	SP2, 	SSS3, 	SPS4, 	SSP5],
+			[SSA7, 	SPA8, 	SS1, 	SP2, 	SSS3, 	SPS4, 	SSP5, 	SPP6],
+			[SPA8, 	SS1, 	SP2, 	SSS3, 	SPS4, 	SSP5, 	SPP6, 	SSA7]]
+
+Col_1 		= 'Col_1' 		#first two words: Det + N
+Col_2 		= 'Col_2'		#second three words: P + Det + N
+Col_3 		= 'Col_3'		#rest of the string
+Condition 	= 'Condition'	#SS, SP, SSS, SPS, SSP, SPP, SSA, SPA
+Exp_Fill 	= 'Exp_Fill'	#Experimental if test sentence, and Filler if filler
+F_N 		= 'F_N'  		#First Noun (note that this is the second Noun)
+S_N 		= 'S_N'			#Second Noun (note that this is the third Noun if SSS, SSP, SPP)
+
+fields = [Col_1, Col_2, Col_3, Condition, Exp_Fill, F_N, S_N]
 
 def read_items(name, separator):
 	items = []
@@ -26,18 +45,8 @@ def make_rectangle(arr):
 
 	assert len(arr) == 80
 
-	combs = [
-			[SS1, 	SP2, 	SSS3, 	SPS4, 	SSP5, 	SPP6, 	SSA7, 	SPA8],
-			[SP2, 	SSS3, 	SPS4, 	SSP5, 	SPP6, 	SSA7, 	SPA8, 	SS1],
-			[SSS3, 	SPS4, 	SSP5, 	SPP6, 	SSA7, 	SPA8, 	SS1, 	SP2],
-			[SPS4, 	SSP5, 	SPP6, 	SSA7, 	SPA8, 	SS1, 	SP2, 	SSS3],
-			[SSP5, 	SPP6, 	SSA7, 	SPA8, 	SS1, 	SP2, 	SSS3, 	SPS4],
-			[SPP6, 	SSA7, 	SPA8, 	SS1, 	SP2, 	SSS3, 	SPS4, 	SSP5],
-			[SSA7, 	SPA8, 	SS1, 	SP2, 	SSS3, 	SPS4, 	SSP5, 	SPP6],
-			[SPA8, 	SS1, 	SP2, 	SSS3, 	SPS4, 	SSP5, 	SPP6, 	SSA7]]
-
 	lists = [	[], [], [], [],
-			[], [], [], []]
+				[], [], [], []]
 
 	j = 0
 
@@ -61,7 +70,36 @@ def make_rectangle(arr):
 		dict = arr[i]
 		for k in range (0, 8):
 			condition = combs[j][k]
-			lists[k].append({condition: dict[condition]})
+			#e.g., SPS
+
+			item = dict[condition]
+			#e.g., the helicopter for the flights over the canyon
+
+			words = item.split()
+
+			col_1 = ' '.join(words[0:2])
+			#e.g., the helicopter
+
+			col_2 = ' '.join(words[2:5])
+			#e.g, for the flights
+
+			col_3 = ' '.join(words[5:])
+			#e.g, over the canyon
+
+			first_noun = words[4]
+			#e.g., flights
+
+			second_noun = words[7] if len(words) == 8 else 'X'
+			#e.g., canyon
+			
+			lists[k].append({	Col_1		:	col_1,
+								Col_2		:	col_2,
+								Col_3		:	col_3,
+								Condition	:	condition,
+								Exp_Fill	:	'Experimental',
+								F_N			:	first_noun,
+								S_N			:	second_noun
+							})
 
 	for i in range (0, 8):		
 		assert len(lists[i]) == 80
